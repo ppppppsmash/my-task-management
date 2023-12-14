18bfba49d39c744f4c5f8f6a0ea797292936e100
ppppppsmash <pppppp.work@gmail.com>
2023-12-14 22:05:59 +0900

    feat: set marketing dir

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+const MarketingPage = () => {
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <div className="flex items-center justify-center flex-col">
+        <div>
+        
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MarketingPage
