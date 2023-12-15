e97f5057fb78b027d0adcb362b02166b4d2e6e85
ppppppsmash <pppppp.work@gmail.com>
2023-12-15 21:20:48 +0900

    feat: Poppins font

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import localFont from "next/font/local"
+import { Poppins } from "next/font/google"
 import { Medal } from "lucide-react"
 
 import { cn } from "@/lib/utils"
@@ -7,6 +8,13 @@ import { Button } from "@/components/ui/button"
 
 const headingFont = localFont({
   src: "../../public/fonts/font.woff2"
+})
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: [
+    "100", "200", "300", "400", "500", "600", "700", "800", "900"
+  ]
 })
 
 const MarketingPage = () => {
@@ -33,8 +41,11 @@ const MarketingPage = () => {
         </div>
       </div>
 
-      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs
-        md:max-w-2xl text-center mx-auto">
+      <div className={cn(
+        "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs",
+        "md:max-w-2xl text-center mx-auto",
+        textFont.className
+      )}>
         やることを管理するアプリ
       </div>
 
