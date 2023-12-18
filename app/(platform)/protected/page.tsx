c533cfde59aff676c53432fb5dc721602a313b65
ppppppsmash <pppppp.work@gmail.com>
2023-12-18 22:14:01 +0900

    test: get the user id

--- app/(platform)/protected/page.tsx
+++ app/(platform)/protected/page.tsx
@@ -1,12 +1,15 @@
+"use client"
 // ログイン後ページ
-import { currentUser } from "@clerk/nextjs"
+import { auth, currentUser } from "@clerk/nextjs"
 
 const ProtectedPage = async () => {
   const user = await currentUser()
+  const { userId } = auth()
   
   return (
     <div>
       User: {user?.firstName}
+      UserId: {userId}
     </div>
   )
 }
