8d93a2639a8c6688395c121e88b97b36e2781a79
ppppppsmash <pppppp.work@gmail.com>
2023-12-19 22:13:59 +0900

    feat: dashboard page navbar

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,12 +1,12 @@
 // http://localhost:3000/organization/xxx
-import { auth } from "@clerk/nextjs"
+import { OrganizationSwitcher, auth } from "@clerk/nextjs"
 
 const OrganizationIdPage = () => {
   const { userId, orgId } = auth()
 
   return (
     <div>
-      OrganizationId: { orgId }
+      <OrganizationSwitcher />
     </div>
   )
 }
