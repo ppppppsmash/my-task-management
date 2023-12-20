f10e5d7f085f12c8a38a660ca0b80bc92aa8dca4
ppppppsmash <pppppp.work@gmail.com>
2023-12-20 22:56:34 +0900

    feat: organization layout

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -6,7 +6,7 @@ const OrganizationIdPage = () => {
 
   return (
     <div>
-      <OrganizationSwitcher />
+      Organization Page
     </div>
   )
 }
---
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -0,0 +1,13 @@
+const OrganizationLayout = ({
+  children
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <main className="pt-20">
+      {children}
+    </main>
+  )
+}
+
+export default OrganizationLayout
