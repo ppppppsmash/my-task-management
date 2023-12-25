e388d9d7538f378ebc3d7a1e7417034757c9b914
ppppppsmash <pppppp.work@gmail.com>
2023-12-25 22:34:34 +0900

    feat: add sidebar nav item

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { Skeleton } from "@/components/ui/skeleton"
 import { Accordion } from "@/components/ui/accordion"
+import { NavItem, Organization } from "@/app/(platform)/(dashboard)/_components/nav-item"
 
 interface SidebarProps {
   storageKey?: string
@@ -89,9 +90,13 @@ export const Sidebar = ({
         className="space-y-2"
       >
         {userMemberships.data.map(({ organization }) => (
-          <p key={organization.id}>
-            {organization.id}
-          </p>
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
         ))}
       </Accordion>
     </>
