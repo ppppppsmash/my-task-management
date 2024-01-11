e591a2e505dd16bb4b253766ea799ae132b3bcfe
ppppppsmash <pppppp.work@gmail.com>
2024-01-11 20:35:54 +0900

    feat: settings component

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -17,6 +17,7 @@ import {
 } from "@/components/ui/accordion"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export type Organization = {
   id: string
@@ -116,5 +117,17 @@ export const NavItem = ({
         ))}
       </AccordionContent>
     </AccordionItem>
+  )
+}
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+
+      <Skeleton className="h-10 w-full" />
+    </div>
   )
 }
