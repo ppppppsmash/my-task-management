9df8383b13e631a4abff27332d62e2af8b72878f
ppppppsmash <pppppp.work@gmail.com>
2024-01-26 20:10:15 +0900

    feat: board list component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,17 @@
 import { Info } from "@/app/(platform)/(dashboard)/_components/info"
+import { Separator } from "@/components/ui/separator"
+import { BoardList } from "@/app/(platform)/(dashboard)/_components/board-list"
 
 const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
+
+      <Separator className="my-4" />
+
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   )
 }
