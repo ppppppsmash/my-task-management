8a82681b4182e80b9a45e0cf3a599bc8680dca25
ppppppsmash <pppppp.work@gmail.com>
2024-02-09 21:02:48 +0900

    feat: board slug page

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { startCase } from "lodash"
+import { auth } from "@clerk/nextjs"
+
 import { OrgControl } from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control"
+
+export async function generateMetadata() {
+  const { orgSlug } = auth()
+
+  return {
+    title: startCase(orgSlug || "organization")
+  }
+}
 
 const OrganizationIdLayout = ({
   children
