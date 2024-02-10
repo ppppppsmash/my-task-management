a4c70decd3d471f83ca64305b524c9b0db6ef8c2
ppppppsmash <pppppp.work@gmail.com>
2024-02-10 17:38:42 +0900

    feat: ready fot board title form

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,7 +1,18 @@
-export const BoardNavbar = () => {
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs"
+import { Board } from "@prisma/client"
+
+interface BoardNavbarProps {
+  data: Board
+}
+
+export const BoardNavbar = async ({
+  data
+}: BoardNavbarProps) => {
+
   return (
-    <div>
-      Board Navbar!
+    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
+      test test
     </div>
   )
 }
