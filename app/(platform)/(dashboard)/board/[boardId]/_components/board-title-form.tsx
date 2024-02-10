150141506ba85a50b19821cc5a03ffc31979090e
ppppppsmash <pppppp.work@gmail.com>
2024-02-10 18:22:09 +0900

    feat: update-board's server actions

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -4,6 +4,9 @@ import { ElementRef, useRef, useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Board } from "@prisma/client"
 import { FormInput } from "@/components/form/form-input"
+import { updateBoard } from "@/actions/update-board"
+import { useAction } from "@/hooks/use-action"
+import { toast } from "sonner"
 
 interface BoardTitleFormProps {
   data: Board
@@ -12,16 +15,28 @@ interface BoardTitleFormProps {
 export const BoardTitleForm = ({
   data
 }: BoardTitleFormProps) => {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board "${data.title}" 更新された!`)
+      setTitle(data.title)
+      disableEditing()
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  })
+
   const formRef = useRef<ElementRef<"form">>(null)
   const inputRef = useRef<ElementRef<"input">>(null)
 
+  const [title, setTitle] = useState(data.title)
   const [isEditing, setIsEditing] = useState(false)
 
   const enableEditing = () => {
     setIsEditing(true)
     setTimeout(() => {
       inputRef.current?.focus()
-      inputRef.current?.select()
+      inputRef.current?.select() 
     })
   }
 
@@ -31,7 +46,11 @@ export const BoardTitleForm = ({
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string
-    console.log("submitted", title)
+
+    execute({
+      title,
+      id: data.id
+    })
   }
 
   const onBlur = () => {
@@ -49,7 +68,7 @@ export const BoardTitleForm = ({
           id="title"
           ref={inputRef}
           onBlur={onBlur}
-          defaultValue={data.title}
+          defaultValue={title}
           className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent
             border-none"
         />
@@ -63,7 +82,7 @@ export const BoardTitleForm = ({
       variant="transparent"
       className="font-bold text-lg h-auto w-auto p-1 px-2"
     >
-      {data.title}
+      {title}
     </Button>
   )
 }
