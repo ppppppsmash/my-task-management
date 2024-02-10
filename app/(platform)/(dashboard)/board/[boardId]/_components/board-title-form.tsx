31732d86c83557c5f3058b6debf4eae061bd6ad7
ppppppsmash <pppppp.work@gmail.com>
2024-02-10 17:58:49 +0900

    feat: onBlur event

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -29,18 +29,29 @@ export const BoardTitleForm = ({
     setIsEditing(false)
   }
 
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string
+    console.log("submitted", title)
+  }
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit()
+  }
+
   if (isEditing) {
     return (
       <form
+        action={onSubmit}
         ref={formRef}
         className="flex items-center gap-x-2"
       >
         <FormInput
           id="title"
           ref={inputRef}
-          onBlur={() => {}}
+          onBlur={onBlur}
           defaultValue={data.title}
-          className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent"
+          className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent
+            border-none"
         />
       </form>
     )
