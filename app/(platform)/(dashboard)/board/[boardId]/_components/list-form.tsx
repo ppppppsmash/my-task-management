97ca7124ee7532db96ac858de347790a45cf185b
ppppppsmash <pppppp.work@gmail.com>
2024-02-13 21:51:24 +0900

    feat: list form component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,9 +1,19 @@
 "use client"
 
+import { Plus } from "lucide-react"
+import { ListWrapper } from "./list-wrapper"
+
 export const ListForm = () => {
   return (
-    <div>
-      List Form!
-    </div>
+    <ListWrapper>
+      <button
+        className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex
+          items-center font-medium text-sm"
+      >
+        <Plus className="h-4 w-4 mr-2" />
+        リストを追加
+      </button>
+    </ListWrapper>
+    
   )
 }
