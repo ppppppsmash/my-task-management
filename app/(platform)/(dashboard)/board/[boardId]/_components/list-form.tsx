d766d84b468b0aa1f3d40bf901cd55e25e0754a7
ppppppsmash <pppppp.work@gmail.com>
2024-02-13 22:01:19 +0900

    feat: list form event

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,12 +1,60 @@
 "use client"
 
 import { Plus } from "lucide-react"
+import { useState, useRef, ElementRef } from "react"
+
 import { ListWrapper } from "./list-wrapper"
+import { useEventListener, useOnClickOutside } from "usehooks-ts"
+import { FormInput } from "@/components/form/form-input"
 
 export const ListForm = () => {
+  const formRef = useRef<ElementRef<"form">>(null)
+  const inputRef = useRef<ElementRef<"input">>(null)
+
+  const [isEditing, setIsEditing] = useState(false)
+
+  const enableEditing = () => {
+    setIsEditing(true)
+    setTimeout(() => {
+      inputRef.current?.focus()
+    })
+  }
+
+  const disableEditing = () => {
+    setIsEditing(false)
+  }
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing()
+    }
+  }
+
+  useEventListener("keydown", onKeyDown)
+  useOnClickOutside(formRef, disableEditing)
+
+  if (isEditing) {
+    return (
+      <ListWrapper>
+        <form
+          ref={formRef}
+          className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
+        >
+          <FormInput
+            ref={inputRef}
+            id="title"
+            className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
+            placeholder="タイトルを入力してください..."
+          />
+        </form>
+      </ListWrapper>
+    )
+  }
+
   return (
     <ListWrapper>
       <button
+        onClick={enableEditing}
         className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex
           items-center font-medium text-sm"
       >
