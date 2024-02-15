dddf530148bbd427a7bea771680899c2f6fa5bb9
ppppppsmash <pppppp.work@gmail.com>
2024-02-15 22:31:50 +0900

    style: list header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,9 +1,20 @@
 "use client"
 
-export const ListHeader = () => {
+import { useState, useRef, ElementRef } from "react"
+import { List } from "@prisma/client"
+
+interface ListHeaderProps {
+  data: List
+}
+
+export const ListHeader = ({
+  data
+}: ListHeaderProps) => {
+
   return (
-    <div>
-      List Header
+    <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
+      <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
+      </div>
     </div>
   )
 }
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -14,8 +14,8 @@ export const ListItem = ({
 }: ListItemProps) => {
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
-      <div className="w-full rounded-md bg-[#1f1f2f4] shadow-md pb-20">
-        <ListHeader />
+      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
+        <ListHeader data={data} />
       </div>
     </li>
   )
