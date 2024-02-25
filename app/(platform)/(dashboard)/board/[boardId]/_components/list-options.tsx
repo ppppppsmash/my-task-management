fa37f768efe04dde0cd1db0eb2b61b3bc0b32695
ppppppsmash <pppppp.work@gmail.com>
2024-02-25 21:48:17 +0900

    style: styling the list options

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -2,6 +2,17 @@
 
 import { List } from "@prisma/client"
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverClose
+} from "@/components/ui/popover"
+import { Button } from "@/components/ui/button"
+import { MoreHorizontal, X } from "lucide-react"
+import { FormSubmit } from "@/components/form/form-submit"
+import { Separator } from "@/components/ui/separator"
+
 interface ListOptionsProps {
   data: List
   onAddCard: () => void
@@ -12,8 +23,68 @@ export const ListOptions = ({
   onAddCard
 }: ListOptionsProps) => {
   return (
-    <div>
-      ListOptions
-    </div>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          className="h-auto w-auto p-2"
+          variant="ghost"
+        >
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent
+        className="px-0 pt-3 pb-3"
+        side="bottom"
+        align="start"
+      >
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          List actions
+        </div>
+
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+
+        <Button
+          onClick={onAddCard}
+          className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          variant="ghost"
+        >
+          カードを追加...
+        </Button>
+
+        <form>
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            variant="ghost"
+            className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          >
+            リストを複製...
+          </FormSubmit>
+        </form>
+
+        <Separator />
+
+        <form>
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            variant="ghost"
+            className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          >
+            リストを削除...
+          </FormSubmit>
+        </form>
+
+
+      </PopoverContent>
+    </Popover>
   )
 }
