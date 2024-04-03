f94ade398fe6bc5d37b52e85fa775d1650023038
ppppppsmash <pppppp.work@gmail.com>
2024-04-03 23:28:23 +0900

    feat: add the forwardRef to child component

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
+import { forwardRef } from "react"
+
 interface CardFormProps {
   listId: string
   enableEditing: () => void
@@ -7,15 +11,25 @@ interface CardFormProps {
   isEditing: boolean
 }
 
-export const CardForm = ({
+export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(({
   listId,
   enableEditing,
   disableEditing,
   isEditing
-}: CardFormProps) => {
+}, ref) => {
   return (
-    <div>
-      Card Form
+    <div className="pt-2 px-2">
+      <Button
+        onClick={enableEditing}
+        className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm"
+        size="sm"
+        variant="ghost"
+      >
+        <Plus className="h-4 w-4 mr-2" />
+        Add a card
+      </Button>
     </div>
   )
-}
+})
+
+CardForm.displayName = "CardForm"
