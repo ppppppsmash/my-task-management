5eaa7670cf00f673ed3f7a2ad317e3e13d824cc8
ppppppsmash <pppppp.work@gmail.com>
2024-04-04 17:14:40 +0900

    feat: drag&drop the card

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { ListWithCards } from "@/types"
-
-import { ListForm } from "./list-form"
 import { useEffect, useState } from "react"
+import { DragDropContext, Droppable } from "@hello-pangea/dnd"
+
+import { ListWithCards } from "@/types"
+import { ListForm } from "./list-form"
 import { ListItem } from "./list-item"
 
 interface ListContainerProps {
@@ -22,17 +23,29 @@ export const ListContainer = ({
   }, [data])
 
   return (
-    <ol className="flex gap-x-3 h-full">
-      {orderedData.map((list, index) => (
-        <ListItem
-          key={list.id}
-          index={index}
-          data={list}
-        />
-      ))}
+    <DragDropContext onDragEnd={() => {}}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => (
+              <ListItem
+                key={list.id}
+                index={index}
+                data={list}
+              />
+            ))}
+            {provided.placeholder}
 
-      <ListForm />
-      <div className="flex-shrink-0 w-1" />
-    </ol>
+            <ListForm />
+
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
