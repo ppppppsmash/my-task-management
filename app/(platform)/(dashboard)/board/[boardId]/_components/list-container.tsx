9abe35adce5f3900e16ce56ce28bebac35cb6d77
ppppppsmash <pppppp.work@gmail.com>
2024-04-04 21:20:42 +0900

    feat: drag&drop logic

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -12,6 +12,14 @@ interface ListContainerProps {
   boardId: string
 }
 
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
+}
+
 export const ListContainer = ({
   data,
   boardId
@@ -21,6 +29,97 @@ export const ListContainer = ({
   useEffect(() => {
     setOrderedData(data)
   }, [data])
+
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result
+
+    if (!destination) {
+      return
+    }
+
+    // if dropped in the same position
+    if (destination.droppableId === source.droppabledId && destination.index === source.index) {
+      return
+    }
+
+    // User moves a list
+    if (type === "list") {
+      const items = reorder(
+        orderedData,
+        source.index,
+        destination.index
+      ).map((item, index) => ({
+        ...item,
+        order: index
+      }))
+
+      setOrderedData(items)
+    }
+
+    // User moves a card
+    if (type === "card") {
+      let newOrderedData = [...orderedData]
+
+      // Source and destination list
+      const sourceList = newOrderedData.find(list => list.id === source.droppabledId)
+      const destList = newOrderedData.find(list => list.id === destination.droppabledId)
+
+      if (!sourceList || !destList) {
+        return
+      }
+
+      // Check if cards exists on the sourceList
+      if (!sourceList.cards) {
+        sourceList.cards = []
+      }
+
+      // Check if cards exists on the destList
+      if (!destList.cards) {
+        destList.cards = []
+      }
+
+      // Moving the card in the same list
+      if (source.droppableId === destination.droppableId) {
+        const reorderedCards = reorder(
+          sourceList.cards,
+          source.index,
+          destination.index
+        )
+
+        reorderedCards.forEach((card, idx) => {
+          card.order = idx
+        })
+
+        sourceList.cards = reorderedCards
+
+        setOrderedData(newOrderedData)
+
+        // Trigger Server Action
+        // User moves the card to another list
+      } else {
+        // Remove card from the source list
+        const [movedCard] = sourceList.cards.splice(source.index, 1)
+
+        // Assign the new listId to the moved card
+        movedCard.listId = destination.droppableId
+
+        // Add card to the destination list
+        destList.cards.splice(destination.index, 0, movedCard)
+
+        sourceList.cards.forEach((card, idx) => {
+          card.order = idx
+        })
+
+        // Update the order for each card in the destination list
+        destList.cards.forEach((card, idx) => {
+          card.order = idx
+        })
+
+        setOrderedData(newOrderedData)
+        // Trigger Server Action
+      }
+    }
+  }
 
   return (
     <DragDropContext onDragEnd={() => {}}>
