aa655b17b7beed1ef1ca28d0a035a5d9856353d9
ppppppsmash <pppppp.work@gmail.com>
2024-04-05 11:49:09 +0900

    feat: react-query & api route

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "sonner"
 
 import { ClerkProvider } from "@clerk/nextjs"
 import { ModalProvider } from "@/components/providers/modal-provider"
+import { QueryProvider } from "@/components/providers/query-provider"
 
 const PlatformLayout = ({
   children
@@ -10,9 +11,11 @@ const PlatformLayout = ({
 }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   )
 }
