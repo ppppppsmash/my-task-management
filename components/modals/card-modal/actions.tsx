773c6c8056d26e8038fae36f4a5dd5d04ddea620
ppppppsmash <pppppp.work@gmail.com>
2024-04-25 21:35:06 +0900

    feat: finish the card actions

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { Copy, Trash } from "lucide-react"
+
 import { CardWithList } from "@/types"
 import { useAction } from "@/hooks/use-action"
 import { copyCard } from "@/actions/copy-card"
@@ -7,7 +9,9 @@ import { deleteCard } from "@/actions/delete-card"
 
 import { Skeleton } from "@/components/ui/skeleton"
 import { Button } from "@/components/ui/button"
-import { Copy, Trash } from "lucide-react"
+import { useParams } from "next/navigation"
+import { useCardModal } from "@/hooks/use-card-modal"
+import { toast } from "sonner"
 
 interface ActionsProps {
   data: CardWithList
@@ -16,6 +20,53 @@ interface ActionsProps {
 export const Actions = ({
   data
 }: ActionsProps) => {
+  const params = useParams()
+  const cardModal = useCardModal()
+
+  const {
+    execute: executeCopyCard,
+    isLoading: isLoadingCopy
+  } = useAction(copyCard, {
+    onSuccess: (data) => {
+      toast.success(`カード "${data.title}" が複製されました.`)
+      cardModal.onClose()
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  })
+
+  const {
+    execute: executeDeleteCard,
+    isLoading: isLoadingDelete
+  } = useAction(deleteCard, {
+    onSuccess: (data) => {
+      toast.success(`カード "${data.title}" が削除されました.`)
+      cardModal.onClose()
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  })
+
+  const onCopy = () => {
+    const boardId = params.boardId as string
+
+    executeCopyCard({
+      id: data.id,
+      boardId
+    })
+  }
+
+  const onDelete = () => {
+    const boardId = params.boardId as string
+
+    executeDeleteCard({
+      id: data.id,
+      boardId
+    })
+  }
+
   return (
     <div className="space-y-2 mt-2">
       <p className="text-xs font-semibold">
@@ -23,6 +74,8 @@ export const Actions = ({
       </p>
 
       <Button
+        onClick={onCopy}
+        disabled={isLoadingCopy}
         variant="gray"
         className="w-full justify-start"
         size="inline"
@@ -32,6 +85,8 @@ export const Actions = ({
       </Button>
 
       <Button
+        onClick={onDelete}
+        disabled={isLoadingDelete}
         variant="gray"
         className="w-full justify-start"
         size="inline"
