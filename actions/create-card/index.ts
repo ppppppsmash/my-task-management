e8254671d277805afb3b4bf45f4892fc9371ec9d
ppppppsmash <pppppp.work@gmail.com>
2024-04-26 22:53:21 +0900

    feat: create audit logs

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -1,11 +1,14 @@
 "use server"
 
 import { auth } from "@clerk/nextjs"
-import { InputType, ReturnType } from "./types"
-import { db } from "@/lib/db"
+import { ACTION, ENTITY_TYPE } from "@prisma/client"
 import { revalidatePath } from "next/cache"
+
+import { db } from "@/lib/db"
+import { InputType, ReturnType } from "./types"
 import { createSafeAction } from "@/lib/create-safe-action"
 import { CreateCard } from "./schema"
+import { createAuditLog } from "@/lib/create-audit-log"
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth()
@@ -49,6 +52,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         listId,
         order: newOrder
       }
+    })
+
+    await createAuditLog({
+      entityId: card.id,
+      entityTitle: card.title,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.CREATE
     })
   } catch (error) {
     return {
