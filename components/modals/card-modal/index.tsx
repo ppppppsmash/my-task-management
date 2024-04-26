1396b41fc38336db4c73938ae662cd938c8d9475
ppppppsmash <pppppp.work@gmail.com>
2024-04-26 23:06:47 +0900

    feat: activity component

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useQuery } from "@tanstack/react-query"
+import { AuditLog } from "@prisma/client"
 
 import { Dialog, DialogContent } from "@/components/ui/dialog"
 import { useCardModal } from "@/hooks/use-card-modal"
@@ -10,6 +11,7 @@ import { fetcher } from "@/lib/fetcher"
 import { Header } from "./header"
 import { Description } from "./description"
 import { Actions } from "./actions"
+import { Activity } from "./activity"
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id)
@@ -19,6 +21,11 @@ export const CardModal = () => {
   const { data: cardData } = useQuery<CardWithList>({
     queryKey: ["card", id],
     queryFn: () => fetcher(`/api/cards/${id}`)
+  })
+
+  const { data: auditLogsData } = useQuery<AuditLog>({
+    queryKey: ["card-logs", id],
+    queryFn: () => fetcher(`/api/cards/${id}/logs`)
   })
 
   return (
@@ -38,6 +45,10 @@ export const CardModal = () => {
               {!cardData
                 ? <Description.Skeleton />
                 : <Description data={cardData} />
+              }
+              {!auditLogsData
+                ? <Activity.Skeleton />
+                : <Activity items={auditLogsData} />
               }
             </div>
           </div>
